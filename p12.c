18f94e1cc4cbf7528b7a41c7339c34e5e670dc14
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2025-12-01 19:06:35 +0200

    coded

---
+++ p12.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+int main()
+{
+    unsigned int n, i, ok = 0, j, k, cnt, cnt_raspuns = 0;
+    bool flag = true, repeta;
+    char s[100], first[100];
+    printf("introduceti numarul de cuvinte : ");
+    scanf("%u", &n);
+    getchar();
+    printf("introduceti cuvintele pe rand : ");
+    for(i = 0; i < n; i++)
+    {
+        repeta = true; // variabila pentru a nu il verifica pe primul cu primul pe linia 26
+        fgets(s, 100, stdin);
+        s[strcspn(s, "\n")] = '\0';
+        if(ok == 0)
+        {
+            strcpy(first, s);
+            ok = 1;
+            repeta = false;
+        }
+        if(ok == 1 && repeta == true)
+        {
+            if(strlen(s) == strlen(first))
+            {
+                cnt = 0;
+                for(j = 0; j < strlen(first); j++)
+                {
+                    flag = true;
+                    for(k = 0; k < strlen(s) && flag == true; k++)
+                        if(first[j] == s[k])
+                        {
+                            cnt++;
+                            flag = false;
+                        }
+                }
+                if(cnt == strlen(first))
+                    cnt_raspuns++;
+            }
+        }
+    }
+    printf("numarul de cuvinte anagrame cu primul cuvant este : %u\n", cnt_raspuns);
+    return 0;
+}
