685768a99bfcdb58877c7b5b2358f64edf679fcd
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2025-12-01 15:00:23 +0200

    coded

---
+++ p1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char s[100];
+    // printf("%s", s);
+    gets(s);
+    printf("sirul de caractere : %s\n", s);
+    return 0;
+}
